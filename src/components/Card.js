ca994f77e6a1ad74294ea74f8b49c2161c6d8f56
Dave Calderwood <davidhcalderwood@gmail.com>
2019-01-19 19:20:50 -0700

    added styling and functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, CardDeck, Button, Col } from 'reactstrap';
-// import data from '../index';
+  CardTitle, CardSubtitle, CardDeck, Col } from 'reactstrap';
+// import Data from '../data/index.json';
 
 const ItemCard = (props) => {
-
-    fetch('./items/index.json')
-//   .then((res) => res.json())
-    .then((data) => {
-    console.log('data:', data);
-  })
-
   return (
     <div>
-    <CardDeck className="deck">
-      <Card className="cards">
-      <Col>
-      <CardTitle className="title">{ props.items.name }</CardTitle><br/>
+        {/* <div>
+        {Data.map((items, index) => {
+            return <div>
+                {items.name}
+                {items.brand}
+                {items.description}
+                {items.shortDescription}
+                {items.imageURL}
+            </div>
+        })}
+        </div> */}
+    <CardDeck className="deck flip-box">
+      <Card className="cards flip-box-inner">
+      <Col className="flip-box-front">
+      <CardTitle className="title">  props.items.name  </CardTitle><br/>
         <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
         <CardBody>
           <CardSubtitle>Brand: Mertz - Welch</CardSubtitle>
           <CardText>Occaecati laborum voluptates. Voluptatem natus sun...</CardText>
-          <Button>See Full Description</Button>
+          <CardText className="desc">Click to see full description</CardText>
+          <div className="flip-box-back">
+          Occaecati laborum voluptates. Voluptatem natus sun…tis reprehenderit exercitationem explicabo magni.
+          </div>
         </CardBody>
         </Col>
       </Card>
