672da2c5c6e2c5c6aee5cfe48913c09da4d85b6a
Dave Calderwood <davidhcalderwood@gmail.com>
2019-01-19 20:58:55 -0700

    Fixed alignment issues and hardcoded data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,37 +1,288 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, CardDeck, Col } from 'reactstrap';
-// import Data from '../data/index.json';
+  CardTitle, CardSubtitle, CardDeck, Container } from 'reactstrap';
 
 const ItemCard = (props) => {
   return (
     <div>
-        {/* <div>
-        {Data.map((items, index) => {
-            return <div>
-                {items.name}
-                {items.brand}
-                {items.description}
-                {items.shortDescription}
-                {items.imageURL}
-            </div>
-        })}
-        </div> */}
+
     <CardDeck className="deck flip-box">
+
       <Card className="cards flip-box-inner">
-      <Col className="flip-box-front">
-      <CardTitle className="title">  props.items.name  </CardTitle><br/>
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Handcrafted Frozen Bacon </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Harum dignissimos placeat fugiat et qui. Accusanti…t omnis. Ipsum voluptatem dicta voluptatem nobis.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Licensed Fresh Keyboard </CardTitle><br/>
         <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
         <CardBody>
           <CardSubtitle>Brand: Mertz - Welch</CardSubtitle>
-          <CardText>Occaecati laborum voluptates. Voluptatem natus sun...</CardText>
-          <CardText className="desc">Click to see full description</CardText>
-          <div className="flip-box-back">
-          Occaecati laborum voluptates. Voluptatem natus sun…tis reprehenderit exercitationem explicabo magni.
-          </div>
+          <CardText>Occaecati laborum voluptates. Voluptatem natus sun…tis reprehenderit exercitationem explicabo magni.</CardText>
         </CardBody>
-        </Col>
+        </Container>
       </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Fantastic Granite Fish </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Ut qui ipsa. Aut iste voluptas cupiditate. Aliquid…usantium eaque corporis. Et alias iste nam error.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Small Soft Gloves </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Quos excepturi doloremque tempore mollitia. Quia a…atis dolor et rerum dolore commodi voluptas eius.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Rustic Cotton Pants </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Dolorem natus eaque debitis illum qui vel impedit …on et sapiente error voluptatem officia et velit.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Handmade Wooden Shirt </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Blick - Spinka</CardSubtitle>
+          <CardText>Sit atque velit nulla eveniet quia repellendus. No…landitiis sunt esse alias sed placeat laboriosam.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Fantastic Metal Shirt </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Delectus dolore ipsam. Et aut enim sit accusantium…equatur aliquam ea nemo accusantium. Vero qui ad.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Tasty Fresh Chicken </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Ut et ipsam consequatur. Et enim et ab qui harum q…it distinctio voluptatem ut. Eum praesentium qui.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Generic Metal Computer </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Quam eum accusamus qui odit eveniet sunt inventore…am animi voluptas facilis et inventore est ullam.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Practical Fresh Hat </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Shields - Christiansen</CardSubtitle>
+          <CardText>Autem quia cumque qui perspiciatis cumque dignissi…. Est fuga tempore quas voluptatem voluptates ad.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Small Concrete Chicken </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: O'Hara - Bradtke</CardSubtitle>
+          <CardText>Est dolores magni nam sed. Facere voluptas enim ne…io quos eligendi eos porro molestias possimus in.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Rustic Rubber Table </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Franecki, Vandervort and Mertz</CardSubtitle>
+          <CardText>Aperiam est numquam tempore. Nihil et at consectet…. Nihil magnam nisi inventore voluptate sunt aut.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Sleek Fresh Bike </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Corkery, Hickle and Schamberger</CardSubtitle>
+          <CardText>Fugit et et quis est quidem non consequatur. Ut bl…iae ut. Ad perferendis itaque nesciunt assumenda.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Unbranded Rubber Shirt </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: McClure Inc</CardSubtitle>
+          <CardText>Maxime non eaque impedit et pariatur fugiat dolore…s aut et. Omnis veritatis quibusdam mollitia sed.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Sleek Cotton Shirt </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Cupiditate deleniti rerum architecto consequatur v…quia minima delectus quidem. Illum ut quidem aut.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Practical Cotton Chips </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: O'Connell, Ryan and Friesen</CardSubtitle>
+          <CardText>Architecto at magni. Earum animi corporis incidunt numquam. Aliquam recusandae explicabo.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Intelligent Metal Shoes </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Minus exercitationem eos adipisci. Unde inventore …lorem vel molestiae velit porro voluptatum sequi.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Generic Plastic Computer </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Doloribus asperiores soluta. Consectetur aut quae. Perferendis molestiae distinctio sit odit.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Ergonomic Soft Keyboard </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Iusto dolore sunt magnam necessitatibus nesciunt t…. Vel rerum dolor maxime rerum sint quia aperiam.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Handcrafted Granite Cheese </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Accusamus accusantium in consequatur sequi quam. E…Enim saepe ut aut officia molestias nihil itaque.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Intelligent Frozen Cheese </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Ledner, Gulgowski and Swift</CardSubtitle>
+          <CardText>Et est voluptatum quas dignissimos et. Porro rerum… vel qui distinctio fugit voluptas est vel atque.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Rustic Steel Gloves </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Ut laudantium doloribus. Officia soluta velit ex d…e aliquam ducimus facilis laboriosam praesentium.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Unbranded Fresh Computer </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Doloribus sit aliquam id ad explicabo. Fugit aut u…di molestiae architecto sit et voluptas minus id.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Licensed Wooden Bike </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: N/A</CardSubtitle>
+          <CardText>Assumenda dolores id omnis alias vitae illo volupt… voluptas molestiae atque aut. A blanditiis modi.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
+      <Card className="cards flip-box-inner">
+      <Container className="flip-box-front">
+      <CardTitle className="title"> Unbranded Frozen Bacon </CardTitle><br/>
+        <CardImg top width="100%" src="https://placebeard.it/400x400" alt="Card image cap" />
+        <CardBody>
+          <CardSubtitle>Brand: Eichmann, Schroeder and Cummerata</CardSubtitle>
+          <CardText>Deserunt est soluta nobis impedit placeat quia iur…st dolor ipsam incidunt. Optio saepe rerum vitae.</CardText>
+        </CardBody>
+        </Container>
+      </Card>
+
     </CardDeck>
     </div>
   );
